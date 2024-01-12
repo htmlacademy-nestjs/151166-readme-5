7e876c36417c8f260609904c914eae7d889e81a7
maratdev <voredev@gmail.com>
2024-01-12 15:32:48 +0300

    Фильтры, валидация и авторизация

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -1,4 +1,9 @@
-import { Logger } from '@nestjs/common';
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
@@ -12,10 +17,16 @@ async function bootstrap() {
     .setDescription('Users service API')
     .setVersion('1.0')
     .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
 
   const configService = app.get(ConfigService);
   const port = configService.get('application.port');
@@ -23,4 +34,5 @@ async function bootstrap() {
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
+
 bootstrap();
--- project/apps/blog/src/app/blog-category/blog-category.entity.ts
+++ project/apps/blog/src/app/blog-category/blog-category.entity.ts
@@ -1,33 +1,35 @@
 import { Category } from '@project/shared/app/types';
 import { Entity } from '@project/shared/core';
 
-export class BlogCategoryEntity implements Category, Entity<string> {
+export class BlogCategoryEntity implements Category, Entity<string, Category> {
   public id?: string;
   public title: string;
   public createdAt?: Date;
   public updatedAt?: Date;
-
   constructor(data: Category) {
     if (!data.title) {
       throw new Error('Category title is required');
     }
-
     this.populate(data);
   }
 
   public populate(data: Category): void {
-    this.id = data.id ?? '';
+    this.id = data.id ?? undefined;
     this.title = data.title;
     this.updatedAt = data.updatedAt ?? undefined;
     this.createdAt = data.createdAt ?? undefined;
   }
 
-  public toPOJO(): Record<string, unknown> {
+  public toPOJO(): Category {
     return {
       id: this.id,
       title: this.title,
       createdAt: this.createdAt,
       updatedAt: this.updatedAt,
     }
+  }
+
+  static fromObject(data: Category): BlogCategoryEntity {
+    return new BlogCategoryEntity(data);
   }
 }
